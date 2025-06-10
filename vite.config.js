f7080370d9c4d23361281551f35cb6a01c811073
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-06-10 23:27:42 +0530

    Remove gh-pages dependency and update Vite base path for local development

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react_router_page/',
+  base: './',
   plugins: [react()],
 });
