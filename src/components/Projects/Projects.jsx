6cdc807637c4406adeadb97f84065b1502318574
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-06-29 20:32:11 +0530

    fix: remove emoji from Projects component header

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -21,7 +21,7 @@ function Github() {
     >
       <div className="py-16 px-4 max-w-7xl mx-auto">
         <h1 className="text-4xl  text-center text-amber-900 dark:text-orange-300 mb-12">
-          🚀 My Projects
+        My Projects
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
