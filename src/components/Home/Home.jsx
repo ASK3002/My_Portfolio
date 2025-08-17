2c8c6aea79aaffee24332e1105b4245d70014ac0
Ayush Singh Kaushik <ayushsinghkaushik111@gmail.com>
2025-08-17 07:56:29 +0530

    resume links

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -77,7 +77,7 @@ export default function Home() {
             </span>
 
             <a
-              href="https://drive.google.com/file/d/1Jefr0Ne8OLpvzUiqUCs-UEwz8fDdKBqG/view?usp=sharing"
+              href="https://drive.google.com/file/d/1p0eng7nd8p8DdvdKP5BoWBoFUco2BRlk/view?usp=sharing"
               target="_blank"
               className="p-3 rounded-full bg-blue-600 hover:bg-blue-700 text-white transition"
               title="View Resume"
@@ -87,7 +87,7 @@ export default function Home() {
             </a>
 
             <a
-              href="https://drive.google.com/uc?export=download&id=1Jefr0Ne8OLpvzUiqUCs-UEwz8fDdKBqG"
+              href="https://drive.google.com/uc?export=download&id=1p0eng7nd8p8DdvdKP5BoWBoFUco2BRlk"
               download
               className="p-3 rounded-full bg-green-600 hover:bg-green-700 text-white transition"
               title="Download Resume"
